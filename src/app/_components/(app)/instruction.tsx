c9241e4ccf2d1a29451b18cef175a4c0c6364eeb
Dimitri Lhopital <dimitrilho@gmail.com>
2024-02-15 17:26:23 +0400

    style: 💄 increase size to font

--- src/app/_components/(app)/instruction.tsx
+++ src/app/_components/(app)/instruction.tsx
@@ -6,7 +6,7 @@ const Instruction = ({ content }: InstructionProps) => {
     <div className="w-full overflow-auto bg-foreground from-background to-foreground p-2 dark:bg-gradient-to-r lg:h-screen lg:w-1/2 lg:p-8">
       <div
         dangerouslySetInnerHTML={{ __html: content }}
-        className="prose w-full max-w-none p-2 pb-16 dark:prose-invert"
+        className="prose w-full max-w-none p-2 pb-16 text-lg dark:prose-invert"
       />
     </div>
   )
