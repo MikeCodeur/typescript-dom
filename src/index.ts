eda4f921c3a4908a1d3b2f8947e3c33662aa8e58
Dimitri Lhopital <dimitrilho@gmail.com>
2024-06-25 13:49:53 +0400

    feat: ✨ example with one url

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export const configModuleName = "Csspro Test" as const
 export const iconUrl =
-  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" as const
+  "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" as const
