4961db33eea2df67c3e3d9aa5ea41cb12fbdd161
Dimitri Lhopital <dimitrilho@gmail.com>
2024-07-01 12:26:42 +0400

    feat: ✨example comment

--- src/app/final/02.tsx
+++ src/app/final/02.tsx
@@ -1,3 +1,6 @@
+// Props Functions
+// 🚀 Calculer sur un événement
+// http://localhost:3000/alone/final/01.bonus-1.js
 export default function Final() {
   return <div>Ceci est la solution 02</div>
 }
