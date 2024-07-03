e425385a95c1d4acb5e54fad9610f3b3ef41e955
Dimitri Lhopital <dimitrilho@gmail.com>
2024-07-03 17:41:31 +0400

    fix: 🐛 center items

--- src/app/_components/(app)/(practice)/pratice-content.tsx
+++ src/app/_components/(app)/(practice)/pratice-content.tsx
@@ -17,17 +17,25 @@ const PracticeContent = ({
     isClient && (
       <div
         className={
-          "m-0 flex h-auto min-h-[100vh] flex-col items-center justify-center overflow-y-auto bg-white p-0"
+          "m-0 flex h-auto min-h-[100vh] w-full flex-col items-center justify-center overflow-y-auto p-0"
         }
       >
-        <div className="w-full max-w-5xl">
-          <div id="root" className="reset-tailwind">
-            {typeof Content === "string" && (
-              <div dangerouslySetInnerHTML={{ __html: Content }} />
-            )}
-            {typeof Content === "function" && <Content />}
-            {typeof Content === "object" && <>{Content}</>}
-          </div>
+        <div
+          id="root"
+          className="reset-tailwind"
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {typeof Content === "string" && (
+            <div dangerouslySetInnerHTML={{ __html: Content }} />
+          )}
+          {typeof Content === "function" && <Content />}
+          {typeof Content === "object" && <>{Content}</>}
         </div>
       </div>
     )
