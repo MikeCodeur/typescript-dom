b0f4aa25a4774052c5ad2dd5d50c3c859e700464
Dimitri Lhopital <dimitrilho@gmail.com>
2024-07-08 15:36:24 +0400

    refactor: 🎨add default name

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export const configModuleName = "Csspro Test" as const
+export const configModuleName = "Course App" as const
 export const iconUrl =
   "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" as const
